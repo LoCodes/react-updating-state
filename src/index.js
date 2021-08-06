da092b8a047a8e6b73f99cdabd0bf72a1ea49ef6
Loren Cipriano <loren25cipriano@gmail.com>
2021-08-06 16:09:32 -0700

    learns to toggle switch & update state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ClickityClick from './components/ClickityClick';
+import LightSwitch from './components/LightSwitch';
+
 
 ReactDOM.render(
   <div>
+    <ClickityClick />
     Mount Components Here
+
+    <LightSwitch />
   </div>,
   document.getElementById('root')
 );
